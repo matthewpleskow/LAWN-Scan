361d384bb35a7e521c9fdae6ebd605cbae26d164
Matthew Pleskow <matthewpleskow@gmail.com>
2023-03-13 16:59:57 -0400

    runs in parallel and stores ips in array

--- runPingScan.sh
+++ runPingScan.sh
@@ -2,19 +2,19 @@
 
 # ISS enabled subnets
 subnets=(
-    "128.61.0.0/20"
-    "128.61.16.0/20"
-    "128.61.32.0/20"
-    "128.61.48.0/20"
-    "128.61.64.0/20"
-    "128.61.112.0/20"
-    "143.215.48.0/20"
-    "143.215.80.0/20"
-    "143.215.96.0/20"
-    "143.215.112.0/20"
-    "128.61.80.0/20"
-    "100.70.32.0/19"
-    "100.70.0.0/19"
+    "128.61.0.0/24"
+    "128.61.16.0/24"
+    "128.61.32.0/24"
+    "128.61.48.0/24"
+    #"128.61.64.0/20"
+    #"128.61.112.0/20"
+    #"143.215.48.0/20"
+    #"143.215.80.0/20"
+    #"143.215.96.0/20"
+    #"143.215.112.0/20"
+    #"128.61.80.0/20"
+    #"100.70.32.0/19"
+    #"100.70.0.0/19"
 )
 
 current_time=$(date +%s%3N)
@@ -26,6 +26,20 @@ do
     filename=nmap_output_${IP_prefix}_${current_time}
 
     echo "Running ping scan on subnet $subnet"
-    output=$(./performPingScan.sh "$subnet")
-    echo $output > outputs/${filename}
+    ./performPingScan.sh "$subnet" > outputs/${filename} &
+    pids+=($!)
 done
+printf '%s\n' "${pids[@]}"
+wait
+for subnet in "${subnets[@]}"
+do
+    IP_prefix=${subnet%%/*}
+    filename=nmap_output_${IP_prefix}_${current_time}	
+    readarray -t ips < "outputs/$filename"
+done
+for ip in "${ips[@]}"
+do
+	echo $ip
+done
+
+
